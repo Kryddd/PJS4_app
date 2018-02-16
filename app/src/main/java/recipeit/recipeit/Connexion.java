8e40ea7dcd25d2a4e65f0a7b94b1161d571b317c
neluth <elenaa.t@hotmail.fr>
2018-02-16 15:13:19 +0100

    connexion

--- app/src/main/java/recipeit/recipeit/Connexion.java
+++ app/src/main/java/recipeit/recipeit/Connexion.java
@@ -1,7 +1,9 @@
 package recipeit.recipeit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Connexion extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Connexion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+    }
+
+    public void creerCompte(View view){
+        Intent creer = new Intent(Connexion.this, inscription.class);
+        startActivity(creer);
     }
 }
